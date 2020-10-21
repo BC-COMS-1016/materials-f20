0831c4ee630c2b58b8ddd34885bee8caf0bdfa03
Adam Poliak <azpoliak@gmail.com>
2020-10-21 17:26:58 -0400

    lab01 for students

---
+++ labs/lab01/tests/q2_1_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_1_1',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> # It looks like you forgot to assign a value to sine_of_pi_over_four;\n>>> assert sine_of_pi_over_four != ...\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
