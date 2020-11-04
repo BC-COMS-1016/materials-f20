cf34757c835eb11e5b3f0fb4d3b9c9a8925195c6
Adam Poliak <azpoliak@gmail.com>
2020-11-03 23:02:27 -0500

    hw05: added untested hw - might be buggy, completed while watching the election results

---
+++ homeworks/hw05/tests/q2_6.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_6',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> assert round(t_chance, 4) == 0.0781\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
