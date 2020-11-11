c67fb2f1b77d6b18991bf12482afc1a3863bc544
Adam Poliak <azpoliak@gmail.com>
2020-11-11 08:05:26 -0500

    project1: addressed priya's comments for public release

--- projects/project1/tests/q1_12.py
+++ projects/project1/tests/q1_12.py
@@ -1,7 +1,7 @@
 test = {   'name': 'q1_12',
     'points': 2,
     'suites': [   {   'cases': [   {'code': ">>> # Check the spelling of the column names;\n>>> assert pop_by_decade.labels == ('decade', 'population')\n", 'hidden': False, 'locked': False},
-                                   {'code': ">>> # The first year of the 1960's is 1960;\n>>> pop_by_decade.column(0).item(0) == 1960\nTrue", 'hidden': False, 'locked': False}],
+                                   {'code': ">>> # The first year of the 1960's is 1960;\n>>> assert pop_by_decade.column(0).item(0) == 1960\n", 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
