a44767c8c0b38dd4f04b20087ff1c51abb9c8ea2
Priya Raj <Priya@Priyas-MacBook-Air.local>
2020-11-19 00:19:34 -0500

    HW06 - final

--- homeworks/hw06/tests/q2_5.py
+++ homeworks/hw06/tests/q2_5.py
@@ -1,1 +1,3 @@
-test = {'name': 'q2_5', 'points': 1, 'suites': [{'cases': [{'code': '>>> assert 0 <= p_value < 1\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'q2_5',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> assert (0 <= p_value < 1) == True\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
