471563be736d67ba9a4523f645557bea4ac44b2f
Adam Poliak <azpoliak@gmail.com>
2020-11-29 12:08:39 -0500

    hw09: added feedback, fixed 2_1 test

--- homeworks/hw09/tests/q2_1.py
+++ homeworks/hw09/tests/q2_1.py
@@ -1,10 +1,9 @@
 test = {   'name': 'q2_1',
     'points': 2,
-    'suites': [   {   'cases': [   {   'code': '>>> type(career_length_slope) in set([float, np.float32, np.float64]) and \\\n'
+    'suites': [   {   'cases': [   {   'code': '>>> assert type(career_length_slope) in set([float, np.float32, np.float64]) and \\\n'
                                                '... type(salary_slope) in set([float, np.float32, np.float64]) and \\\n'
                                                '... type(career_length_intercept) in set([float, np.float32, np.float64]) and \\\n'
-                                               '... type(salary_intercept) in set([float, np.float32, np.float64])\n'
-                                               'True',
+                                               '... type(salary_intercept) in set([float, np.float32, np.float64])\n',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
