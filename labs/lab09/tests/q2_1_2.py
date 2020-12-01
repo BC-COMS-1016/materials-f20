08d087e324b3e49df223923be6f0b5332bf47919
Priya Raj <pdrajbabu@gmail.com>
2020-12-01 00:00:48 -0500

    lab09

---
+++ labs/lab09/tests/q2_1_2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_1_2',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> assert(np.isclose(probability_large_given_shiny, 4/10))\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
