06f947f68a82bda9037c996d4ce7d40c84f7fdbf
Adam Poliak <azpoliak@gmail.com>
2020-12-03 00:57:38 -0500

    addressed fran's and priya's feedback

--- homeworks/hw08/tests/q1_4.py
+++ homeworks/hw08/tests/q1_4.py
@@ -1,5 +1,5 @@
 test = {   'name': 'q1_4',
-    'points': 1,
+    'points': 2,
     'suites': [   {   'cases': [{'code': '>>> assert int(correlation([1,2,3], [4,5,6]) * 10000) / 10000 == 0.9999\n', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
