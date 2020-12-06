88ba2f7140e5f10c1e1480da85f4a6bc6e4dc5e3
Adam Poliak <azpoliak@gmail.com>
2020-12-06 18:11:28 -0500

    hw09: addressed priya's comments

--- homeworks/hw09/tests/q1_12.py
+++ homeworks/hw09/tests/q1_12.py
@@ -1,3 +1,10 @@
 test = {   'name': 'q1_12',
     'points': 1,
-    'suites': [{'cases': [{'code': '>>> assert type(reject_sal) == bool\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [   {   'cases': [   {   'code': '>>> assert type(lower_bound_sal) in set([float, np.float16, np.float32, np.float64]);\n'
+                                               '>>> assert type(upper_bound_sal) in set([float, np.float16, np.float32, np.float64])\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
